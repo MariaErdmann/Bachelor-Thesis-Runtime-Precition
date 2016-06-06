7aa0f2cdba74629fae5a3329292f70832cb1559d
Maria Erdmann <maria.erdmann@campus.lmu.de>
2016-06-06 11:05:08 +0200

    Update .batchtools_conf.R

--- .batchtools_conf.R
+++ .batchtools_conf.R
@@ -1,5 +1,5 @@
-#cluster.functions = makeClusterFunctionsSLURM(template.file = "lmu_lrz.tmpl", 
-#                                              list.jobs.cmd = c("squeue", "-h", "-o %i", "-u $USER", 
-#                                              "--clusters=serial", "| tail -n +2"))
-#cluster.functions = makeClusterFunctionsMulticore()
-#debug = TRUE
+cluster.functions = batchtools::makeClusterFunctionsSLURM("~/lmu_lrz_new.tmpl", clusters = 
+    "mpp2")
+default.resources = list(walltime = 300L, memory = 512L, ntasks = 28L)
+
+debug = TRUE
