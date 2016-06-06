53ece0d451a86e30dc5106feb0d620aa748151dc
Maria Erdmann <maria.erdmann@campus.lmu.de>
2016-06-06 11:14:27 +0200

    Update experiments.R

--- experiments.R
+++ experiments.R
@@ -1,20 +1,22 @@
 library(mlr)
 library(batchtools)
 library(plyr)
+library(OpenML)
 
-dir = "C:/Users/Maria/Documents/Studium/Statistik/Bachelorarbeit"
+setOMLConfig(apikey = "6f5535ee9d1e819c0f85447006bca0c3", arff.reader = "farff")
+
+#dir = "~/code/"
+dir = "C:/Users/Maria/Documents/Studium/Statistik/Bachelorarbeit/Code_neu"
 setwd(paste0(dir,"/Results"))
-source(paste0(dir,"/Code/definitions.R"))
+source(paste0(dir,"/definitions.R"))
 
 unlink("runtime-try", recursive = TRUE)
 regis = makeExperimentRegistry("runtime-try", 
-  packages = c("mlr", "OpenML", "randomForest", "methods"),
-  source = "C:/Users/Maria/Documents/Studium/Statistik/Bachelorarbeit/Code/definitions.R",
-  work.dir = "C:/Users/Maria/Documents/Studium/Statistik/Bachelorarbeit/results",
-  conf.file = "C:/Users/Maria/Documents/Studium/Statistik/Bachelorarbeit.R"
+  packages = c("mlr", "OpenML", "ranger", "methods"),
+  source = "../definitions.R",
+  work.dir = ".",
+  conf.file = "../"
 )
-
-#regis$cluster.functions = makeClusterFunctionsMulticore() 
 
 # add our selected OML dsets as problems
 for (did in OMLDATASETS) {
@@ -22,26 +24,29 @@ for (did in OMLDATASETS) {
   addProblem(name = as.character(did), data = data)
 }
 
-# add one generic 'algo' that evals the RF in hyperpar space
+# add one generic 'algo' that evals the algos in hyperpar space
 addAlgorithm("eval", fun = function(job, data, instance, lrn.id, ...) {
   par.vals = list(...)
   oml.dset = getOMLDataSet(data$did)             
   task = convertOMLDataSetToMlr(oml.dset)
   type = getTaskType(task)
+  bin.or.multi = length(task$task.desc$class.levels)
   par.vals = par.vals[!(is.na(par.vals))]
   par.vals = CONVERTPARVAL(par.vals, task, lrn.id)
+  sub.sample.frac = par.vals[["sub.sample.frac"]]
+  par.vals = par.vals[names(par.vals) != "sub.sample.frac"]
   lrn.id = paste0(type, ".", lrn.id)
-  lrn = switch(type, "classif" = makeLearner(lrn.id), "regr" = makeLearner(lrn.id))
+  lrn = makeLearner(lrn.id)
   lrn = setHyperPars(lrn, par.vals = par.vals)
   measures = MEASURES
-  mod = train(lrn, task)
-  p = predict(mod, task)
-  performance(pred = p, measures = measures, model = mod)
-  # statt predict und performance
-  #subsample(lrn, task, split = 2/3, models = TRUE, keep.pred = FALSE, meausres = measures)
+  #mod = train(lrn, task)
+  #p = predict(mod, task)
+  #list(performance(pred = p, measures = measures, model = mod), lrn)
+  # instead of predict() und performance()
+  subsample(lrn, task, iters = 1L, split = sub.sample.frac, stratify = FALSE, models = TRUE, keep.pred = FALSE, measures = measures)
 })
 
-# Random maximin design
+# Random design
 set.seed(124)
 ades = data.frame()
 for (lid in LEARNERIDS) {
@@ -53,41 +58,33 @@ for (lid in LEARNERIDS) {
 }
 addExperiments(algo.designs = list(eval = ades))
 
-# FIXME: we need to add the defaults of each learner and defaults that we could invent.
+# FIXME: If  I need to add the defaults of each learner
 # defaults
-ades_def = data.frame()
-for (lid in "ranger") {
-  ps = makeMyDefaultParamSet(lid)
-  d = generateGridDesign(ps, resolution = 1)
-  d = cbind(lrn.id = lid, d, stringsAsFactors = FALSE)
-  ades_def = rbind.fill(ades_def, d)
-}
+# ades_def = data.frame()
+# for (lid in "ranger") {
+#   ps = makeMyDefaultParamSet(lid)
+#   d = generateGridDesign(ps, resolution = 1)
+#   d = cbind(lrn.id = lid, d, stringsAsFactors = FALSE)
+#   ades_def = rbind.fill(ades_def, d)
+# }
 
 summarizeExperiments()
 ids = getJobTable()$job.id
-ids = c(1:3,11:13,33:36,49:51,60:63,82:84,96:98,107:108)
+ids = c(1:3,10:13,25:27,43:44,100:103,151:152)
 
 
-
-# Paritions jobs into chunks of size 100, these will be executed together
-#ids = chunkIds(findNotDone(ids), chunk.size = 100)
-
+# Auskoemmentieren für Cluseter
 submitJobs(ids)
-
-# checken was das ist:
-# submitJobs(ids, resources = list(chunk.ncpus = 9))
-
 getStatus()
 getErrorMessages()
 
 # Results
-res = reduceResultsDataTable(ids, fun = function(r) as.data.frame(as.list(r)), reg = regis)
-res
-hyper_pars = getJobPars(ids)
-hyper_pars
-
-hyper.pars.time = merge(hyper_pars, res, by = "job.id")
-save(hyper.pars.time, file = "C:/Users/Maria/Documents/Studium/Statistik/Bachelorarbeit/results/hyper.pars.time.RData")
+# res = reduceResultsList(ids, fun = function (r) r, reg = regis)
+# Does not produce a data table any more because of the class of subsample, I guess
+# res = reduceResultsDataTable(1, fun = function(r) as.data.frame(as.list(r)), reg = regis)
+# res
+# hyper_pars = getJobPars(ids)
+# hyper_pars
 
 # zu Debugzwecken
 #lrn.id = "ranger"
