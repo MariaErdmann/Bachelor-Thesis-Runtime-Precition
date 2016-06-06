1d363d1a251e9c8d397e01be6deafd8206b8b66a
Maria Erdmann <maria.erdmann@campus.lmu.de>
2016-06-06 11:12:24 +0200

    Delete getResults.R
    deprecated

--- getResults.R
+++
@@ -1,9 +0,0 @@
-# Get results for BatchExperiments
-res1 = reduceResultsExperiments(myreg, ids = findDone(myreg), fun = function(job, res) res$aggr)
-res2 = reduceResultsExperiments(myreg, ids = findDone(myreg), fun = function(job, res) res$measures.test)
-
-
-
-# Get Result for batchtools::batchmark
-res3 = reduceResults(ids = 1, fun = function(job,res) res$model)
-
