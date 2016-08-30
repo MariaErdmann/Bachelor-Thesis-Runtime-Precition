a8d83674be4bf760780685525f25e9815d73b704
Maria Erdmann <maria.erdmann@campus.lmu.de>
2016-08-30 12:14:49 +0200

    Delete plausibilityCheck.R

--- plausibilityCheck.R
+++
@@ -1,8 +0,0 @@
-# check if there are any time measurements missing and if meausurements are reasonable
-anyNA(res$timetrain.test.mean)
-anyNA(res$timepredict.test.mean)
-anyNA(res$timeboth.test.mean)
-summary(res$timetrain.test.mean)
-summary(res$timepredict.test.mean)
-summary(res$timeboth.test.mean)
-
